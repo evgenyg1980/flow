2667b1168776a5bb6df166d749ebd14c30767a0b
evgenyg1980 <evgenyg1980@gmail.com>
2025-05-11 11:35:10 +0300

    server.py

--- server.py
+++ server.py
@@ -110,4 +110,7 @@ def download_file(filename):
     return send_from_directory(OUTPUT_FOLDER, filename)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
