a06acce65d207b240ee38cd6f3f8c07f2186e84c
evgenyg1980 <evgenyg1980@gmail.com>
2025-05-11 11:47:24 +0300

    Update server.py

--- server.py
+++ server.py
@@ -108,8 +108,3 @@ def split_status():
 def download_file(filename):
     return send_from_directory(OUTPUT_FOLDER, filename)
 
-# הגדרה שמכבדת את $PORT שמוגדר על ידי Railway
-if __name__ != "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
