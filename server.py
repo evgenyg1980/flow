2f0c9d2bcd130b823f54e852148f2be832370cb7
evgenyg1980 <evgenyg1980@gmail.com>
2025-05-11 13:20:08 +0300

    Update server.py

--- server.py
+++ server.py
@@ -112,6 +112,4 @@ def download_file(filename):
 def health():
     return jsonify({"status": "ok"}), 200
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+
